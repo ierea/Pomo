77f6a1766951532327d59c1cbcff93bdf4748cd9
ierea <106348521+ierea@users.noreply.github.com>
2022-05-28 06:44:22 +1000

    Set up minimum window size

--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -3,10 +3,12 @@ using System;
 
 public class Main : Node
 {
+    [Export] private Vector2 MinimumWindowSize;
+    
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+        OS.MinWindowSize = MinimumWindowSize;
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
