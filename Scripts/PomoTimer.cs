e9c0c37873f258565b6091a3a6aa2e1c59dd6d68
ierea <106348521+ierea@users.noreply.github.com>
2022-05-29 17:39:24 +1000

    Set up reset to default buttons for each option

--- Scripts/PomoTimer.cs
+++ Scripts/PomoTimer.cs
@@ -23,16 +23,27 @@ public class PomoTimer : Control
 
     [Export] private NodePath OptionsPopupNodePath;
 
+    [Export] private NodePath OptionsResetSfxVolumeButtonNodePath;
     [Export] private NodePath OptionsSfxVolumeSliderNodePath;
 
+    [Export] private NodePath OptionsResetUpperTimerColorButtonNodePath;
     [Export] private NodePath OptionsUpperTimerColorPickerButtonNodePath;
-    [Export] private NodePath OptionsLowerTimerColorPickerButtonNodePath;
     [Export] private NodePath OptionsUpperTimerColorPickerTextureRectNodePath;
+
+    [Export] private NodePath OptionsResetLowerTimerColorButtonNodePath;
+    [Export] private NodePath OptionsLowerTimerColorPickerButtonNodePath;
     [Export] private NodePath OptionsLowerTimerColorPickerTextureRectNodePath;
 
+    [Export] private NodePath OptionsResetWorkTimerDurationButtonNodePath;
     [Export] private NodePath OptionsWorkTimerDurationLineEditNodePath;
+
+    [Export] private NodePath OptionsResetShortBreakTimerDurationButtonNodePath;
     [Export] private NodePath OptionsShortBreakTimerDurationLineEditNodePath;
+
+    [Export] private NodePath OptionsResetLongBreakTimerDurationButtonNodePath;
     [Export] private NodePath OptionsLongBreakTimerDurationLineEditNodePath;
+
+    [Export] private NodePath OptionsResetLongBreakFrequencyButtonNodePath;
     [Export] private NodePath OptionsLongBreakFrequencyLineEditNodePath;
 
     [Export] private Texture PauseTexture;
@@ -65,18 +76,34 @@ public class PomoTimer : Control
     private TextureRect LowerTimerTextureRect;
     private Button PinWindowButton;
     private TextureRect PinWindowButtonTextureRect;
+
     private OptionsPopup OptionsPopup;
+
+    private Button OptionsResetSfxVolumeButton;
     private HSlider OptionsSfxVolumeSlider;
+    
+    private Button OptionsResetTimerUpperColorButton;
     private ColorPickerButton OptionsTimerUpperColorPickerButton;
-    private ColorPickerButton OptionsTimerLowerColorPickerButton;
     private TextureRect OptionsTimerUpperColorPickerTextureRect;
+
+    private Button OptionsResetTimerLowerColorButton;
+    private ColorPickerButton OptionsTimerLowerColorPickerButton;
     private TextureRect OptionsTimerLowerColorPickerTextureRect;
+
+    private Button OptionsResetWorkTimerDurationButton;
     private LineEdit OptionsWorkTimerDurationLineEdit;
+
+    private Button OptionsResetShortBreakTimerDurationButton;
     private LineEdit OptionsShortBreakTimerDurationLineEdit;
+
+    private Button OptionsResetLongBreakTimerDurationButton;
     private LineEdit OptionsLongBreakTimerDurationLineEdit;
+
+    private Button OptionsResetLongBreakFrequencyButton;
     private LineEdit OptionsLongBreakFrequencyLineEdit;
 
     private UserPreferences userPreferences;
+    private UserPreferences defaultUserPreferences;
 
     private Phase currentPhase;
     private int currentMinutesRemaining;
@@ -90,11 +117,9 @@ public class PomoTimer : Control
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
+        defaultUserPreferences = new UserPreferences();
         userPreferences = UserPreferences.CreateFromFile(UserPreferencesFileName);
         ResetTimerValues();
-
-        AudioStreamPlayer = GetNode<AudioStreamPlayer>(AudioStreamPlayerNodePath);
-        SetSfxVolume(userPreferences.SfxVolume);
 
         TimeLabel = GetNode<Label>(TimeLabelNodePath);
         PauseButton = GetNode<Button>(PauseButtonNodePath);
@@ -115,25 +140,35 @@ public class PomoTimer : Control
 
         OptionsPopup = GetNode<OptionsPopup>(OptionsPopupNodePath);
 
-        OptionsSfxVolumeSlider = GetNode<HSlider>(OptionsSfxVolumeSliderNodePath);
-        OptionsSfxVolumeSlider.Value = userPreferences.SfxVolume;
-
+        OptionsResetTimerUpperColorButton = GetNode<Button>(OptionsResetUpperTimerColorButtonNodePath);
         OptionsTimerUpperColorPickerButton = GetNode<ColorPickerButton>(OptionsUpperTimerColorPickerButtonNodePath);
-        OptionsTimerLowerColorPickerButton = GetNode<ColorPickerButton>(OptionsLowerTimerColorPickerButtonNodePath);
         OptionsTimerUpperColorPickerTextureRect = GetNode<TextureRect>(OptionsUpperTimerColorPickerTextureRectNodePath);
+
+        OptionsResetTimerLowerColorButton = GetNode<Button>(OptionsResetLowerTimerColorButtonNodePath);
+        OptionsTimerLowerColorPickerButton = GetNode<ColorPickerButton>(OptionsLowerTimerColorPickerButtonNodePath);
         OptionsTimerLowerColorPickerTextureRect = GetNode<TextureRect>(OptionsLowerTimerColorPickerTextureRectNodePath);
 
+        OptionsResetWorkTimerDurationButton = GetNode<Button>(OptionsResetWorkTimerDurationButtonNodePath);
         OptionsWorkTimerDurationLineEdit = GetNode<LineEdit>(OptionsWorkTimerDurationLineEditNodePath);
         OptionsWorkTimerDurationLineEdit.Text = userPreferences.WorkMinutes.ToString();
 
+        OptionsResetShortBreakTimerDurationButton = GetNode<Button>(OptionsResetShortBreakTimerDurationButtonNodePath);
         OptionsShortBreakTimerDurationLineEdit = GetNode<LineEdit>(OptionsShortBreakTimerDurationLineEditNodePath);
         OptionsShortBreakTimerDurationLineEdit.Text = userPreferences.ShortBreakMinutes.ToString();
 
+        OptionsResetLongBreakTimerDurationButton = GetNode<Button>(OptionsResetLongBreakTimerDurationButtonNodePath);
         OptionsLongBreakTimerDurationLineEdit = GetNode<LineEdit>(OptionsLongBreakTimerDurationLineEditNodePath);
         OptionsLongBreakTimerDurationLineEdit.Text = userPreferences.LongBreakMinutes.ToString();
 
+        OptionsResetLongBreakFrequencyButton = GetNode<Button>(OptionsResetLongBreakFrequencyButtonNodePath);
         OptionsLongBreakFrequencyLineEdit = GetNode<LineEdit>(OptionsLongBreakFrequencyLineEditNodePath);
         OptionsLongBreakFrequencyLineEdit.Text = userPreferences.LongBreakFrequency.ToString();
+
+        AudioStreamPlayer = GetNode<AudioStreamPlayer>(AudioStreamPlayerNodePath);
+        OptionsResetSfxVolumeButton = GetNode<Button>(OptionsResetSfxVolumeButtonNodePath);
+        OptionsSfxVolumeSlider = GetNode<HSlider>(OptionsSfxVolumeSliderNodePath);
+        OptionsSfxVolumeSlider.Value = userPreferences.SfxVolume;
+        SetSfxVolume(userPreferences.SfxVolume);
 
         UpdateTimerText();
         UpdateTimerRectSizes();
@@ -347,7 +382,89 @@ public class PomoTimer : Control
 
     private void OnOptionsButtonPressed()
     {
+        ValidateResetButtons();
         OptionsPopup.ShowOptionsPopup();
+    }
+
+    // Hide reset buttons if values are default
+    private void ValidateResetButtons()
+    {
+        if (Mathf.Abs(userPreferences.SfxVolume - defaultUserPreferences.SfxVolume) < Mathf.Epsilon)
+        {
+            OptionsResetSfxVolumeButton.Visible = false;
+            OptionsResetSfxVolumeButton.Disabled = true;
+        }
+        else
+        {
+            OptionsResetSfxVolumeButton.Visible = true;
+            OptionsResetSfxVolumeButton.Disabled = false;
+        }
+
+        if (userPreferences.UpperTimerColor.ToRgba32() == defaultUserPreferences.UpperTimerColor.ToRgba32())
+        {
+            OptionsResetTimerUpperColorButton.Visible = false;
+            OptionsResetTimerUpperColorButton.Disabled = true;
+        }
+        else
+        {
+            OptionsResetTimerUpperColorButton.Visible = true;
+            OptionsResetTimerUpperColorButton.Disabled = false;
+        }
+
+        if (userPreferences.LowerTimerColor.ToRgba32() == defaultUserPreferences.LowerTimerColor.ToRgba32())
+        {
+            OptionsResetTimerLowerColorButton.Visible = false;
+            OptionsResetTimerLowerColorButton.Disabled = true;
+        }
+        else
+        {
+            OptionsResetTimerLowerColorButton.Visible = true;
+            OptionsResetTimerLowerColorButton.Disabled = false;
+        }
+
+        if (userPreferences.WorkMinutes == defaultUserPreferences.WorkMinutes)
+        {
+            OptionsResetWorkTimerDurationButton.Visible = false;
+            OptionsResetWorkTimerDurationButton.Disabled = true;
+        }
+        else
+        {
+            OptionsResetWorkTimerDurationButton.Visible = true;
+            OptionsResetWorkTimerDurationButton.Disabled = false;
+        }
+
+        if (userPreferences.ShortBreakMinutes == defaultUserPreferences.ShortBreakMinutes)
+        {
+            OptionsResetShortBreakTimerDurationButton.Visible = false;
+            OptionsResetShortBreakTimerDurationButton.Disabled = true;
+        }
+        else
+        {
+            OptionsResetShortBreakTimerDurationButton.Visible = true;
+            OptionsResetShortBreakTimerDurationButton.Disabled = false;
+        }
+
+        if (userPreferences.LongBreakMinutes == defaultUserPreferences.LongBreakMinutes)
+        {
+            OptionsResetLongBreakTimerDurationButton.Visible = false;
+            OptionsResetLongBreakTimerDurationButton.Disabled = true;
+        }
+        else
+        {
+            OptionsResetLongBreakTimerDurationButton.Visible = true;
+            OptionsResetLongBreakTimerDurationButton.Disabled = false;
+        }
+
+        if (userPreferences.LongBreakFrequency == defaultUserPreferences.LongBreakFrequency)
+        {
+            OptionsResetLongBreakFrequencyButton.Visible = false;
+            OptionsResetLongBreakFrequencyButton.Disabled = true;
+        }
+        else
+        {
+            OptionsResetLongBreakFrequencyButton.Visible = true;
+            OptionsResetLongBreakFrequencyButton.Disabled = false;
+        }
     }
 
     private void SaveUserPreferencesToFile()
@@ -375,22 +492,56 @@ public class PomoTimer : Control
         userPreferences.SfxVolume = newVolume;
         AudioStreamPlayer.VolumeDb = GD.Linear2Db(userPreferences.SfxVolume / 100.0f);
         SaveUserPreferencesToFile();
+        ValidateResetButtons();
+    }
+
+    private void OnResetVolumeButtonPressed()
+    {
+        float defaultSfxVolume = defaultUserPreferences.SfxVolume;
+        OptionsSfxVolumeSlider.Value = defaultSfxVolume;
+        SetSfxVolume(defaultSfxVolume);
+
+        ValidateResetButtons();
     }
 
     private void OnUpperTimerColorPickerButtonColorChanged(Color newColor)
+    {
+        SetUpperTimerColor(newColor);
+
+        ValidateResetButtons();
+    }
+
+    private void SetUpperTimerColor(Color newColor)
     {
         userPreferences.UpperTimerColor = newColor;
         UpdateTimerRectColors();
         UpdateColorPickerTextureRects();
         SaveUserPreferencesToFile();
+        ValidateResetButtons();
+    }
+
+    private void OnResetUpperColorButtonPressed()
+    {
+        SetUpperTimerColor(defaultUserPreferences.UpperTimerColor);
     }
 
     private void OnLowerTimerColorPickerButtonColorChanged(Color newColor)
+    {
+        SetLowerTimerColor(newColor);
+    }
+
+    private void SetLowerTimerColor(Color newColor)
     {
         userPreferences.LowerTimerColor = newColor;
         UpdateTimerRectColors();
         UpdateColorPickerTextureRects();
         SaveUserPreferencesToFile();
+        ValidateResetButtons();
+    }
+
+    private void OnResetLowerColorButtonPressed()
+    {
+        SetLowerTimerColor(defaultUserPreferences.LowerTimerColor);
     }
 
     private void OnWorkTimerDurationLineEditTextEntered(string newText)
@@ -403,6 +554,11 @@ public class PomoTimer : Control
         SubmitTimerDuration(OptionsWorkTimerDurationLineEdit, OptionsWorkTimerDurationLineEdit.Text, Phase.Work);
     }
 
+    private void OnResetWorkTimerDurationButtonPressed()
+    {
+        SubmitTimerDuration(OptionsWorkTimerDurationLineEdit, defaultUserPreferences.WorkMinutes.ToString(), Phase.Work);
+    }
+
     private void OnShortBreakTimerDurationLineEditTextEntered(string newText)
     {
         SubmitTimerDuration(OptionsShortBreakTimerDurationLineEdit, newText, Phase.ShortBreak);
@@ -413,6 +569,11 @@ public class PomoTimer : Control
         SubmitTimerDuration(OptionsShortBreakTimerDurationLineEdit, OptionsShortBreakTimerDurationLineEdit.Text, Phase.ShortBreak);
     }
 
+    private void OnResetShortBreakTimerDurationButtonPressed()
+    {
+        SubmitTimerDuration(OptionsShortBreakTimerDurationLineEdit, defaultUserPreferences.ShortBreakMinutes.ToString(), Phase.ShortBreak);
+    }
+
     private void OnLongBreakTimerDurationLineEditTextEntered(string newText)
     {
         SubmitTimerDuration(OptionsLongBreakTimerDurationLineEdit, newText, Phase.LongBreak);
@@ -421,6 +582,11 @@ public class PomoTimer : Control
     private void OnLongBreakTimerDurationLineEditFocusExited()
     {
         SubmitTimerDuration(OptionsLongBreakTimerDurationLineEdit, OptionsLongBreakTimerDurationLineEdit.Text, Phase.LongBreak);
+    }
+
+    private void OnResetLongBreakTimerDurationButtonPressed()
+    {
+        SubmitTimerDuration(OptionsLongBreakTimerDurationLineEdit, defaultUserPreferences.LongBreakMinutes.ToString(), Phase.LongBreak);
     }
 
     private void SubmitTimerDuration(LineEdit OptionsTimerDurationLineEdit, string newText, Phase phaseType)
@@ -489,6 +655,8 @@ public class PomoTimer : Control
         }
 
         SaveUserPreferencesToFile();
+
+        ValidateResetButtons();
         
         // If in a fresh session, reset to apply the new values.
         if (freshSessionAndShouldPlaySfxOnPlay)
@@ -507,6 +675,11 @@ public class PomoTimer : Control
     private void OnLongBreakFrequencyLineEditFocusExited()
     {
         SubmitLongBreakFrequency(OptionsLongBreakFrequencyLineEdit.Text);
+    }
+
+    private void OnResetLongBreakFrequencyButtonPressed()
+    {
+        SubmitLongBreakFrequency(defaultUserPreferences.LongBreakFrequency.ToString());
     }
 
     private void SubmitLongBreakFrequency(string newText)
@@ -537,6 +710,8 @@ public class PomoTimer : Control
         OptionsLongBreakFrequencyLineEdit.Text = userPreferences.LongBreakFrequency.ToString();
 
         SaveUserPreferencesToFile();
+
+        ValidateResetButtons();
         
         // If in a fresh session, reset to apply the new values.
         if (freshSessionAndShouldPlaySfxOnPlay)
