b6c42e3c4cf95f05686134885ca7fd631a3a6c47
ierea <106348521+ierea@users.noreply.github.com>
2022-06-27 17:21:48 +1000

    Improve main class documentation

--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -1,11 +1,16 @@
 using Godot;
 using System;
 
+/// <summary>
+/// Handles basic window logic.
+/// </summary>
 public class Main : Node
 {
     [Export] private Vector2 MinimumWindowSize;
 
-    // Called when the node enters the scene tree for the first time.
+    /// <summary>
+    /// Called when the node enters the scene tree for the first time.
+    /// </summary>
     public override void _Ready()
     {
         OS.MinWindowSize = MinimumWindowSize;
