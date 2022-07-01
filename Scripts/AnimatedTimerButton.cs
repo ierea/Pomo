2e3250e6919d2bfd48152686e05f15bdc438f655
ierea <106348521+ierea@users.noreply.github.com>
2022-07-01 16:41:12 +1000

    Set up animated timer buttons receiver methods summary

--- Scripts/AnimatedTimerButton.cs
+++ Scripts/AnimatedTimerButton.cs
@@ -1,6 +1,9 @@
 using Godot;
 using System;
 
+/// <summary>
+/// Handles animations for animated buttons.
+/// </summary>
 public class AnimatedTimerButton : Button
 {
     [Export] private NodePath AnimationPlayerNodePath;
@@ -13,7 +16,9 @@ public class AnimatedTimerButton : Button
     private AnimationPlayer AnimationPlayer;
     private TextureRect IconTextureRect;
 
-    // Called when the node enters the scene tree for the first time.
+    /// <summary>
+    /// Called when the node enters the scene tree for the first time.
+    /// </summary>
     public override void _Ready()
     {
         AnimationPlayer = GetNode<AnimationPlayer>(AnimationPlayerNodePath);
@@ -22,21 +27,41 @@ public class AnimatedTimerButton : Button
         IconTextureRect.RectPivotOffset = IconTextureRect.RectSize / 2.0f;
     }
 
+    /// <summary>
+    /// Signal Receiver Method.<br/>
+    /// For use with the animated timer button when it is pressed down.<br/>
+    /// Plays the button down animation.
+    /// </summary>
     private void OnAnimatedTimerButtonButtonDown()
     {
         AnimationPlayer.Play(ButtonDownAnimationName);
     }
 
+    /// <summary>
+    /// Signal Receiver Method.<br/>
+    /// For use with the animated timer button when it is released.<br/>
+    /// Plays the button up animation.
+    /// </summary>
     private void OnAnimatedTimerButtonButtonUp()
     {
         AnimationPlayer.Play(HoveredAnimationName);
     }
 
+    /// <summary>
+    /// Signal Receiver Method.<br/>
+    /// For use with the animated timer button when it is hovered.<br/>
+    /// Plays the hovered animation.
+    /// </summary>
     private void OnAnimatedTimerButtonMouseEntered()
     {
         AnimationPlayer.Play(HoveredAnimationName);
     }
 
+    /// <summary>
+    /// Signal Receiver Method.<br/>
+    /// For use with the animated timer button when it is no longer hovered.<br/>
+    /// Plays the normal state animation.
+    /// </summary>
     private void OnAnimatedTimerButtonMouseExited()
     {
         AnimationPlayer.Play(NormalAnimationName);
