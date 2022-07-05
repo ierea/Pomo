10278fc40e7622f5e8a7f1a9b46eb2c5f169e8b6
ierea <106348521+ierea@users.noreply.github.com>
2022-07-05 11:35:52 +1000

    Set up animated color picker button doc summary

--- Scripts/AnimatedColorPickerButton.cs
+++ Scripts/AnimatedColorPickerButton.cs
@@ -1,6 +1,9 @@
 using Godot;
 using System;
 
+/// <summary>
+/// Handles animations for animated color picker buttons.
+/// </summary>
 public class AnimatedColorPickerButton : ColorPickerButton
 {
     [Export] private NodePath AnimationPlayerNodePath;
@@ -13,7 +16,9 @@ public class AnimatedColorPickerButton : ColorPickerButton
     private AnimationPlayer AnimationPlayer;
     private TextureRect IconTextureRect;
 
-    // Called when the node enters the scene tree for the first time.
+    /// <summary>
+    /// Called when the node enters the scene tree for the first time.
+    /// </summary>
     public override void _Ready()
     {
         AnimationPlayer = GetNode<AnimationPlayer>(AnimationPlayerNodePath);
@@ -22,21 +27,41 @@ public class AnimatedColorPickerButton : ColorPickerButton
         IconTextureRect.RectPivotOffset = IconTextureRect.RectSize / 2.0f;
     }
 
+    /// <summary>
+    /// Signal Receiver Method.<br/>
+    /// For use with the animated color picker button when it is pressed down.<br/>
+    /// Plays the button down animation.
+    /// </summary>
     private void OnAnimatedColorPickerButtonButtonDown()
     {
         AnimationPlayer.Play(ButtonDownAnimationName);
     }
 
+    /// <summary>
+    /// Signal Receiver Method.<br/>
+    /// For use with the animated color picker button when it is released.<br/>
+    /// Plays the button up animation.
+    /// </summary>
     private void OnAnimatedColorPickerButtonButtonUp()
     {
         AnimationPlayer.Play(HoveredAnimationName);
     }
 
+    /// <summary>
+    /// Signal Receiver Method.<br/>
+    /// For use with the animated color picker button when it is hovered.<br/>
+    /// Plays the hovered animation.
+    /// </summary>
     private void OnAnimatedColorPickerButtonMouseEntered()
     {
         AnimationPlayer.Play(HoveredAnimationName);
     }
 
+    /// <summary>
+    /// Signal Receiver Method.<br/>
+    /// For use with the animated color picker button when it is no longer hovered.<br/>
+    /// Plays the normal state animation.
+    /// </summary>
     private void OnAnimatedColorPickerButtonMouseExited()
     {
         AnimationPlayer.Play(NormalAnimationName);
